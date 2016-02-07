c2314c339642c62dea23290293bb8b74c16deb55
Firemark <marpiechula@gmail.com>
2016-02-07 01:08:21 +0100

    add CMP

--- code.asm
+++ code.asm
@@ -1,9 +1,17 @@
-MOV R9 126
+MOV R9 127
 loop: ;prepare loop
     ADD R8 1
     ADD R9 1
-    BRO main
+    BRO _loop2
     RJMP loop
+
+_loop2: ;check CMP
+MOV R8 0
+loop2:
+    ADD R8 1
+    CMP R8 4
+    BREQ main
+    RJMP loop2
 main: 
 MOV RF 0xBE ;print funny text in hex
 MOV RE 0xEF
--- cpu.v
+++ cpu.v
@@ -88,29 +88,36 @@ module cpu (clk, reset, date_bus, adress_bus, r, w, halt);
 	task compute;
 		input byte value;
 		bit old_sign;
+		byte temp;
 	begin
 		$display("%s %s R%h VAL %h (%d)", 
 			operator_group.name(), math_operator.name(), 
 			reg_num, value, value);
-		old_sign <= rg[reg_num][7];
+		old_sign = rg[reg_num][7];
 		case(math_operator)
-			OP_ADD: {carry, rg[reg_num]} <= {1'b0, rg[reg_num]} + {1'b0, value};
-			OP_SUB: {carry, rg[reg_num]} <= {1'b0, rg[reg_num]} - {1'b0, value};
+			OP_ADD: {carry, rg[reg_num]} = {1'b0, rg[reg_num]} + {1'b0, value};
+			OP_SUB: {carry, rg[reg_num]} = {1'b0, rg[reg_num]} - {1'b0, value};
 			OP_ADC:
-                {carry, rg[reg_num]} <= {1'b0, rg[reg_num]} + {1'b0, value} + {8'b0, carry};
+                {carry, rg[reg_num]} = {1'b0, rg[reg_num]} + {1'b0, value} + {8'b0, carry};
 			OP_SBC:
-                {carry, rg[reg_num]} <= {1'b0, rg[reg_num]} - {1'b0, value} - {8'b0, carry};
-			OP_AND: rg[reg_num] <= rg[reg_num] & value;
-			OP_OR: rg[reg_num] <= rg[reg_num] | value;
-			OP_XOR: rg[reg_num] <= rg[reg_num] ^ value;
-			OP_CMP: rg[reg_num] <= 0;
+                {carry, rg[reg_num]} = {1'b0, rg[reg_num]} - {1'b0, value} - {8'b0, carry};
+			OP_AND: rg[reg_num] = rg[reg_num] & value;
+			OP_OR: rg[reg_num] = rg[reg_num] | value;
+			OP_XOR: rg[reg_num] = rg[reg_num] ^ value;
+			OP_CMP: begin
+				{carry, temp} = rg[reg_num] - value;
+				overflow = old_sign ^ temp;
+				zero = temp == 8'b0;
+				negative = temp[7];
+			end
 			OP_MOV: rg[reg_num] <= value;
 			default: rg[reg_num] <= 0;
 		endcase
 		if (math_operator != OP_CMP) begin
-			overflow <= old_sign ^ rg[reg_num][7];
-			zero <= rg[reg_num] == 8'b0;
-			negative <= rg[reg_num][7];
+			overflow = old_sign ^ rg[reg_num][7];
+			zero = rg[reg_num] == 8'b0;
+			negative = rg[reg_num][7];
+			$display("flags: Ov:%b Z:%b N:%b C:%b", overflow, zero, negative, carry);
 		end
 	end endtask
 
@@ -118,32 +125,38 @@ module cpu (clk, reset, date_bus, adress_bus, r, w, halt);
 		input shortint value;
         bit [1:0] ereg_num;
         bit old_sign;
+        shortint temp;
         assign ereg_num = reg_num[2:1];
 	begin
 		$display("%s %s ER%h VAL %h (%d)", 
 			operator_group.name(), math_operator.name(), 
 			ereg_num, value, value);
-		old_sign <= erg[ereg_num][15];
+		old_sign = erg[ereg_num][15];
 		case(math_operator)
 			OP_ADD:
-                {carry, rg[reg_num+1], rg[reg_num]} <= {1'b0, erg[ereg_num]} + {1'b0, value};
+                {carry, rg[reg_num+1], rg[reg_num]} = {1'b0, erg[ereg_num]} + {1'b0, value};
 			OP_SUB:
-                {carry, rg[reg_num+1], rg[reg_num]} <= {1'b0, erg[ereg_num]} - {1'b0, value};
+                {carry, rg[reg_num+1], rg[reg_num]} = {1'b0, erg[ereg_num]} - {1'b0, value};
 			OP_ADC:
-                {carry, rg[reg_num+1], rg[reg_num]} <= {1'b0, erg[ereg_num]} + {1'b0, value} + {16'b0, carry};
+                {carry, rg[reg_num+1], rg[reg_num]} = {1'b0, erg[ereg_num]} + {1'b0, value} + {16'b0, carry};
 			OP_SBC:
-                {carry, rg[reg_num+1], rg[reg_num]} <= {1'b0, erg[ereg_num]} - {1'b0, value} - {16'b0, carry};
-			OP_AND: {rg[reg_num+1], rg[reg_num]} <= erg[ereg_num] & value;
-			OP_OR: {rg[reg_num+1], rg[reg_num]} <= erg[ereg_num] | value;
-			OP_XOR: {rg[reg_num+1], rg[reg_num]} <= erg[ereg_num] ^ value;
-			OP_CMP: {rg[reg_num+1], rg[reg_num]} <= 0;
-			OP_MOV: {rg[reg_num+1], rg[reg_num]} <= value;
-			default: {rg[reg_num+1], rg[reg_num]} <= 0;
+                {carry, rg[reg_num+1], rg[reg_num]} = {1'b0, erg[ereg_num]} - {1'b0, value} - {16'b0, carry};
+			OP_AND: {rg[reg_num+1], rg[reg_num]} = erg[ereg_num] & value;
+			OP_OR: {rg[reg_num+1], rg[reg_num]} = erg[ereg_num] | value;
+			OP_XOR: {rg[reg_num+1], rg[reg_num]} = erg[ereg_num] ^ value;
+			OP_CMP: begin
+				{carry, temp} = erg[ereg_num] - value;
+				overflow = old_sign ^ temp;
+				zero = temp == 16'b0;
+				negative = temp[15];
+			end
+			OP_MOV: {rg[reg_num+1], rg[reg_num]} = value;
+			default: {rg[reg_num+1], rg[reg_num]} = 0;
 		endcase
 		if (math_operator != OP_CMP) begin
-			overflow <= old_sign ^ erg[ereg_num][15];
-			zero <= erg[ereg_num] == 16'b0;
-			negative <= erg[ereg_num][15];
+			overflow = old_sign ^ erg[ereg_num][15];
+			zero = erg[ereg_num] == 16'b0;
+			negative = erg[ereg_num][15];
 		end
 	end endtask
 
