71e7a06b05d2c4afe405619de504d2e09b1fa863
Firemark <marpiechula@gmail.com>
2016-02-08 01:56:24 +0100

    add more instructions

--- cpu_data.v
+++ cpu_data.v
@@ -3,26 +3,16 @@
 
 typedef enum bit [3:0] {
     OP_ADD = 'b0000,
+    OP_ADC = 'b0001,
     OP_SUB = 'b0010,
+    OP_SBC = 'b0011,
     OP_AND = 'b0100,
     OP_OR = 'b0110,
     OP_XOR = 'b1000,
     OP_MOV = 'b1010,
-    OP_CMP = 'b1100,
-    OP_ADC = 'b0001,
-    OP_SBC = 'b0011
+    OP_CMP = 'b1100
     //OP_SWP = 'b1000
 } Math_operator;
-
-typedef enum bit [2:0] {
-    OP_CLC = 'b000,
-    OP_CLZ = 'b001,
-    OP_CLO = 'b010,
-    OP_CLN = 'b011,
-    OP_WTF = 'b100,
-    OP_NOP = 'b110,
-    OP_HLT = 'b111
-} Other_operator;
 
 typedef enum bit [3:0] {
     OP_BREQ = 'b0000, //equality
@@ -37,17 +27,62 @@ typedef enum bit [3:0] {
     OP_BRNN = 'b1001, //not negative
     OP_BRLO = 'b1010, //lower 
     OP_BRSH = 'b1011, //same and higher
-    OP_RJMP = 'b1100 //restricted jump
+    OP_RJMP = 'b1100, //restricted jump
+    OP_RCALL = 'b1101 //restricted call
 } Branch_operator;
 
+typedef enum bit [3:0] {
+    OP_NEG = 'b0000,
+    OP_COM = 'b0001,
+    OP_LSL = 'b0010,
+    OP_LSR = 'b0011,
+    OP_ROL = 'b0100,
+    OP_ROR = 'b0101,
+    OP_RLC = 'b0110,
+    OP_RRC = 'b0111,
+    OP_PUSH = 'b1000,
+    OP_POP = 'b1001
+} Single_operator;
+
 typedef enum bit [2:0] {
+    OP_LD = 'b000,
+    OP_LDI = 'b001,
+    OP_LDD = 'b010,
+    OP_ST = 'b011,
+    OP_STI = 'b100,
+    OP_STD = 'b101
+} Register_memory_operator;
+
+typedef enum bit [3:0] {
+    OP_JMP = 'b0000,
+    OP_CALL = 'b0001
+} Extended_operator;
+
+typedef enum bit [3:0] {
+    OP_CLC = 'b0000,
+    OP_CLZ = 'b0001,
+    OP_CLO = 'b0010,
+    OP_CLN = 'b0011,
+    OP_NOP = 'b0100,
+    OP_HLT = 'b0110,
+    OP_RET = 'b0111,
+    OP_STC = 'b1000,
+    OP_STZ = 'b1001,
+    OP_STO = 'b1010,
+    OP_STN = 'b1011
+} Other_operator;
+
+typedef enum {
     GROUP_MATH_CONSTANT,
     GROUP_MATH_REG,
     GROUP_MATH_EREG,
     GROUP_BRANCH_JUMPS,
+    GROUP_SINGLE_REG,
+    GROUP_REG_MEMORY,
+    GROUP_SINGLE_EREG,
+    GROUP_EXTENDED,
     GROUP_OTHERS,
     GROUP_WRONG
 } Operator_group;
-
 
 `endif
