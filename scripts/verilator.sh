81eb131bd533ef00f1dd657b0c2bb2d27b4ddd08
Firemark <marpiechula@gmail.com>
2016-03-05 01:00:50 +0100

    move main.cpp and change verilator.sh

--- scripts/verilator.sh
+++ scripts/verilator.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 rm -rf obj_dir/
 verilator \
-    --cc cpu.v\
-    --exe main.cpp
+    --cc $DIR/src/cpu.v\
+    -I$DIR/src\
+    --exe $DIR/src-emulator/main.cpp
 cd obj_dir
 make -j -f Vcpu.mk Vcpu
